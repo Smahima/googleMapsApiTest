62731db78cdc4a66dfc752ad1af4b0a40d5d1a33
Your name <Mahima Subramanian>
2017-10-19 11:30:31 -0500

    google maps is rendering

--- src/App.js
+++ src/App.js
@@ -1,21 +1,74 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+import './index.css';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      zoom: 13,
+      maptype: 'roadmap',
+      place_formatted: '',
+      place_id: '',
+      place_location: '',
+    };
+  }
+
+  componentDidMount() {
+    let map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: -33.8688, lng: 151.2195},
+      zoom: 13,
+      mapTypeId: 'roadmap',
+    });
+
+    map.addListener('zoom_changed', () => {
+      this.setState({
+        zoom: map.getZoom(),
+      });
+    });
+
+    map.addListener('maptypeid_changed', () => {
+      this.setState({
+        maptype: map.getMapTypeId(),
+      });
+    });
+
+    let marker = new window.google.maps.Marker({
+      map: map,
+      position: {lat: -33.8688, lng: 151.2195},
+    });
+
+    // initialize the autocomplete functionality using the #pac-input input box
+    let inputNode = document.getElementById('pac-input');
+    map.controls[window.google.maps.ControlPosition.TOP_LEFT].push(inputNode);
+    let autoComplete = new window.google.maps.places.Autocomplete(inputNode);
+
+    autoComplete.addListener('place_changed', () => {
+      let place = autoComplete.getPlace();
+      let location = place.geometry.location;
+
+      this.setState({
+        place_formatted: place.formatted_address,
+        place_id: place.place_id,
+        place_location: location.toString(),
+      });
+
+      // bring the selected place in view on the map
+      map.fitBounds(place.geometry.viewport);
+      map.setCenter(location);
+
+      marker.setPlace({
+        placeId: place.place_id,
+        location: location,
+      });
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id='app'>
+        <div id='map' />
       </div>
     );
   }
-}
-
-export default App;
+};
